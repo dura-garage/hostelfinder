eebccfbf6bf887aedf07ee61337e742a0ffa055a
dura-amar <dura.email@gmail.com>
2022-10-13 08:37:03 +0545

    Committing all previous changes

--- services/hostelservice.h
+++ services/hostelservice.h
@@ -2,10 +2,21 @@
 #define HOSTELSERVICE_H
 
 
+/*
+* The Hostel Service extends the HostelRepository
+* It responds to the requests from the ui classes
+* Define the functions that are to be done with the Hostel
+*
+*/
+
 class HostelService
 {
 public:
     HostelService();
+
+
+
+
 };
 
 #endif // HOSTELSERVICE_H
